677623f572a88f3c1577315be4f5c4fedd36aae0
Mohamed-aldabaan <mohamed.aldabaan@dci-student.org>
2024-08-06 01:47:27 +0200

    update

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,6 +1,18 @@
+import Header from "./components/1-header/Header";
+import Hero from "./components/2-hero/Hero";
+import Main from "./components/3-main/Main";
+import Contact from "./components/4-contact/Contact";
+import Footer from "./components/5-footer/Footer";
 
 function App() {
-  return <></>;
+  return <>
+    <Header />
+    <Hero />
+    <Main />
+    <Contact />
+    <Footer />
+
+  </>;
 }
 
 export default App;
