72fb27c474a11c59566a5d5d298f412ef6d22f93
Mohamed-aldabaan <mohamed.aldabaan@dci-student.org>
2024-08-06 23:53:06 +0200

    bearbeitet

--- my-portfolio/src/components/2-hero/Hero.jsx
+++ my-portfolio/src/components/2-hero/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <section className="hero flex">
       <article className="left-article border">left</article>
-      <article className="right-article border">right</article>
+      <article className="right-article border">animation</article>
     </section>
   );
 }
