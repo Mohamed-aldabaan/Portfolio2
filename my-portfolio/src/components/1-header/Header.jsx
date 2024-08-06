ebff353ebfc0acdc35479bc26393f0bf3e68d92a
Mohamed-aldabaan <mohamed.aldabaan@dci-student.org>
2024-08-06 14:41:00 +0200

    update header

--- my-portfolio/src/components/1-header/Header.jsx
+++ my-portfolio/src/components/1-header/Header.jsx
@@ -4,6 +4,8 @@ import "./header.css";
 export default function Header() {
   return (
     <header className=" flex">
+      <button className="menu">show model</button>
+      <div />
       <nav>
         <ul className="flex">
           <li>
@@ -24,6 +26,26 @@ export default function Header() {
         </ul>
       </nav>
       <button>light</button>
+      <div className="fixed">
+        <ul className="modal  ">
+          <li>close</li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Projects</a>
+          </li>
+          <li>
+            <a href="">Study</a>
+          </li>
+          <li>
+            <a href="">Hobbies</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
