cc2a55eb468fab0908d2a96ec90b8358b1e98579
Mohamed-aldabaan <mohamed.aldabaan@dci-student.org>
2024-08-07 00:12:40 +0200

    update

--- my-portfolio/src/components/2-hero/Hero.jsx
+++ my-portfolio/src/components/2-hero/Hero.jsx
@@ -7,11 +7,20 @@ function Hero() {
       <article className="left-article border">
         <div className="profile-parent">
           <img className="profile" src="/profil.jpg" alt="profil-bild" />
-          <p>
+          <h4>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
             tempore odio architecto expedita, praesentium nulla magni reiciendis
             dolorem omnis, vero exercitationem nesciunt quis fugiat quia.
             Recusandae delectus sint molestias provident.
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+            inventore dolorum quidem nam fugiat ratione tempore ipsa facere
+            velit eius. Quia velit harum maiores! Nesciunt rem minus laudantium
+            eligendi atque voluptatibus minima exercitationem quis optio nam, id
+            ullam dolorem necessitatibus neque dolorum, tempora ab repudiandae
+            natus incidunt veniam suscipit praesentium? Repudiandae labore, sint
+            voluptatibus repellendus alias amet quia eos officia!
           </p>
         </div>
       </article>
