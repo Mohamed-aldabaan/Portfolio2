e389e1e9a841923770a5ada5d7cde77a82a825ba
Mohamed-aldabaan <mohamed.aldabaan@dci-student.org>
2024-08-06 23:56:32 +0200

    update

--- my-portfolio/src/components/2-hero/Hero.jsx
+++ my-portfolio/src/components/2-hero/Hero.jsx
@@ -4,7 +4,11 @@ import "./hero.css";
 function Hero() {
   return (
     <section className="hero flex">
-      <article className="left-article border">left</article>
+      <article className="left-article border">
+        <div className="profile-parent">
+          <img src="/profil.jpg" alt="profil-bild" />
+        </div>
+      </article>
       <article className="right-article border">animation</article>
     </section>
   );
