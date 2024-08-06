b99cc3ae9650b41d4e2117db6185ac6bbb51391f
Mohamed-aldabaan <mohamed.aldabaan@dci-student.org>
2024-08-06 02:37:39 +0200

    update

--- my-portfolio/src/components/1-header/Header.jsx
+++ my-portfolio/src/components/1-header/Header.jsx
@@ -2,5 +2,27 @@ import React from "react";
 import "./header.css";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Projects</a>
+          </li>
+          <li>
+            <a href="">Study</a>
+          </li>
+          <li>
+            <a href="">Hobbies</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
