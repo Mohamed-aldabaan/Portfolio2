9da9d3a70386c08955797d8d5ef3950087072cf3
Mohamed-aldabaan <mohamed.aldabaan@dci-student.org>
2024-08-06 23:21:36 +0200

    update hero section

--- my-portfolio/src/components/2-hero/Hero.jsx
+++ my-portfolio/src/components/2-hero/Hero.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
-import './hero.css';
+import { useState } from "react";
+import "./hero.css";
 
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <section className="hero flex">
+      <article className="left-article border">left</article>
+      <article className="right-article border">right</article>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
