e7f2c5a7b595866eaff1d82058507f32a71a9894
Mohamed-aldabaan <mohamed.aldabaan@dci-student.org>
2024-08-06 16:35:10 +0200

    update header

--- my-portfolio/src/components/1-header/Header.jsx
+++ my-portfolio/src/components/1-header/Header.jsx
@@ -1,10 +1,19 @@
-// import React from "react";
+import React from "react";
 import "./header.css";
+import { useState } from "react";
 
 export default function Header() {
+  const [showModal, setShowModal] = useState(false);
   return (
     <header className=" flex">
-      <button className="menu">show model</button>
+      <button
+        className="menu"
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
+        show model
+      </button>
       <div />
       <nav>
         <ul className="flex">
@@ -26,26 +35,30 @@ export default function Header() {
         </ul>
       </nav>
       <button>light</button>
-      <div className="fixed">
-        <ul className="modal  ">
-          <li>close</li>
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Projects</a>
-          </li>
-          <li>
-            <a href="">Study</a>
-          </li>
-          <li>
-            <a href="">Hobbies</a>
-          </li>
-          <li>
-            <a href="">Contact</a>
-          </li>
-        </ul>
-      </div>
+      {showModal && (
+        <div className="fixed">
+          <ul className="modal  ">
+            <li >
+              <button className="border" onClick={() => setShowModal(false)}>close</button>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Projects</a>
+            </li>
+            <li>
+              <a href="">Study</a>
+            </li>
+            <li>
+              <a href="">Hobbies</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
