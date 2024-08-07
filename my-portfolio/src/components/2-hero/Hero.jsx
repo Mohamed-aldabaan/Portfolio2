61c1d111fd03be55f5b9a689fcf70e661614b066
Mohamed-aldabaan <mohamed.aldabaan@dci-student.org>
2024-08-07 18:35:41 +0200

    update

--- my-portfolio/src/components/2-hero/Hero.jsx
+++ my-portfolio/src/components/2-hero/Hero.jsx
@@ -4,24 +4,26 @@ import "./hero.css";
 function Hero() {
   return (
     <section className="hero flex">
-      <article className="left-article border">
+      <article className="left-article">
         <div className="profile-parent">
           <img className="profile" src="/profil.jpg" alt="profil-bild" />
-          <h4>
+          <h2 className="title">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
             tempore odio architecto expedita, praesentium nulla magni reiciendis
-            dolorem omnis, vero exercitationem nesciunt quis fugiat quia.
-            Recusandae delectus sint molestias provident.
-          </h4>
-          <p>
+          </h2>
+          <p className="sub-title">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
             inventore dolorum quidem nam fugiat ratione tempore ipsa facere
             velit eius. Quia velit harum maiores! Nesciunt rem minus laudantium
             eligendi atque voluptatibus minima exercitationem quis optio nam, id
-            ullam dolorem necessitatibus neque dolorum, tempora ab repudiandae
-            natus incidunt veniam suscipit praesentium? Repudiandae labore, sint
-            voluptatibus repellendus alias amet quia eos officia!
+            ullam dolorem
           </p>
+          <div className="icons flex">
+            <div className="icon icon-github-square"></div>
+            <div className="icon icon-linkedin-square"></div>
+            <div className="icon icon-instagram"></div>
+            <div className="icon icon-x"></div>
+          </div>
         </div>
       </article>
       <article className="right-article border">animation</article>
