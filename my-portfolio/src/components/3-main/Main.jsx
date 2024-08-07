6b937fe844d102c4f1c09fc0ab6ae90c884a0fc7
Mohamed-aldabaan <mohamed.aldabaan@dci-student.org>
2024-08-07 23:13:47 +0200

    update

--- my-portfolio/src/components/3-main/Main.jsx
+++ my-portfolio/src/components/3-main/Main.jsx
@@ -3,6 +3,28 @@ import './main.css';
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <main className='flex'>
+      <section className='left-section flex'>
+        <button>All Projects</button>
+        <button>HTML & CSS</button>
+        <button>JavaScript</button>
+        <button>React</button>
+        <button>Node & Express</button>
+      </section>
+      <section className='right-section border'>
+        <article className='card'>
+          <img src="/p1.jpg" alt="" />
+          <div className="box">
+            <h1 className="title">First Project</h1>
+            <p className="sub-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, quod.</p>
+            <div className="flex icons">
+              <li > <a href="" className="icon-link"></a> </li>
+              <li > <a href="https://github.com/Mohamed-aldabaan" className="icon-github-square" target='_blank'></a> </li>
+              <li > more <a href="" className="icon-arrow-right"></a></li>
+            </div>
+          </div>
+        </article>
+      </section>
+    </main>
   )
 }
