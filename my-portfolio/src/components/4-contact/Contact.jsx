5e2b971e86bb53e83c6cd1279e2e5026b4849e31
Mohamed-aldabaan <mohamed.aldabaan@dci-student.org>
2024-08-08 16:38:12 +0200

    update contact form

--- my-portfolio/src/components/4-contact/Contact.jsx
+++ my-portfolio/src/components/4-contact/Contact.jsx
@@ -6,10 +6,10 @@ export default function Contact() {
       <p> Let us get in touch! Send me a message:</p>
       <div className="flex">
         <form action="" className="flex">
-          <input type="text" name="text" id="text" placeholder='Your name' />
-          <input type="email" id="email" placeholder='Your E-mail' />
-          <textarea name="message" id="" rows={5} placeholder='Your message'/> 
-          <button>Send</button>
+          <input type="text" name="text" id="text" placeholder='Your name' autoComplete="off" required />
+          <input type="email" id="email" placeholder='Your E-mail' autoComplete="off" required/>
+          <textarea name="message" id="message" rows={5} placeholder='Your message' autoComplete="off" required></textarea>
+          <button className='submit'>Send</button>
         </form>
         
         <div className="animation">Animation</div>
