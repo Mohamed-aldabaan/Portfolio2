29b76c03a27fa541766a175755e58dd7f76f192c
Mohamed-aldabaan <mohamed.aldabaan@dci-student.org>
2024-08-08 15:57:28 +0200

    update contact form

--- my-portfolio/src/components/4-contact/Contact.jsx
+++ my-portfolio/src/components/4-contact/Contact.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
 import './contact.css';
 export default function Contact() {
   return (
-    <div>Contact us</div>
+    <section className="contact flex">
+      <h1 className='icon-envelope'> contact me</h1>
+      <p> Let us get in touch! Send me a message:</p>
+      <div className="flex">
+        <form action="" className="flex">
+          <input type="text" name="text" id="text" placeholder='Your name' />
+          <input type="email" id="email" placeholder='Your E-mail' />
+          <textarea name="message" id="" rows={5} placeholder='Your message'/> 
+          <button>Send</button>
+        </form>
+        
+        <div className="animation">Animation</div>
+      </div>
+    </section>
   )
 }
