ef961773123c557f0249d1903b473ab1bb073a7f
Mohamed-aldabaan <mohamed.aldabaan@dci-student.org>
2024-08-08 02:45:12 +0200

    update

--- my-portfolio/src/components/3-main/Main.jsx
+++ my-portfolio/src/components/3-main/Main.jsx
@@ -4,26 +4,28 @@ import './main.css';
 export default function Main() {
   return (
     <main className='flex'>
-      <section className='left-section flex'>
+      <section className='left-section  flex'>
         <button>All Projects</button>
         <button>HTML & CSS</button>
         <button>JavaScript</button>
         <button>React</button>
         <button>Node & Express</button>
       </section>
-      <section className='right-section '>
-        <article className='card'>
-          <img src="/p1.jpg" alt="" />
-          <div className="box">
-            <h1 className="title">First Project</h1>
-            <p className="sub-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, quod.</p>
-            <div className="flex icons">
-              <li > <a href="" className="icon-link"></a> </li>
-              <li > <a href="https://github.com/Mohamed-aldabaan" className="icon-github-square" target='_blank'></a> </li>
-              <li className='flex'> more <a href="" className="icon-arrow-right"></a></li>
+      <section className='right-section flex'>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item, index) => (
+           <article key={index} className='card'>
+            <img src="/p1.jpg" alt="projekt-1" />
+            <div className="box">
+              <h1 className="title">First Project</h1>
+              <p className="sub-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, quod.</p>
+              <div className="flex icons">
+                <li > <a href="" className="icon-link"></a> </li>
+                <li > <a href="https://github.com/Mohamed-aldabaan" className="icon-github-square" target='_blank'></a> </li>
+                <li className='flex'> more <a href="" className="icon-arrow-right"></a></li>
+              </div>
             </div>
-          </div>
-        </article>
+          </article>
+        ))}
       </section>
     </main>
   )
