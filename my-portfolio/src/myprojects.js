d330ab4151117f76b0a37bbe627003fbee9a008d
Mohamed-aldabaan <mohamed.aldabaan@dci-student.org>
2024-08-09 20:00:02 +0200

    update

--- my-portfolio/src/myprojects.js
+++ my-portfolio/src/myprojects.js
@@ -1,6 +1,6 @@
 
 export default  [
-    { id: 1, 'title': 'Projekt 1', 'image': '/p1.jpg', description: 'Beschreibung von Projekt 1', category:'html & css' },
+    { id: 1, 'title': 'Projekt 1', 'image': '/p1.png', description: 'Beschreibung von Projekt 1', category:'html & css' },
     { id: 2, title: 'Projekt 2', image: '/p2.jpg', description: 'Beschreibung von Projekt 2', category:'javascript' },
     { id: 3, title: 'Projekt 3', image: '/p2.jpg', description: 'Beschreibung von Projekt 3', category:'react' },
     { id: 4, title: 'Projekt 4', image: '/p1.jpg', description: 'Beschreibung von Projekt 1', category:'html & css' },
