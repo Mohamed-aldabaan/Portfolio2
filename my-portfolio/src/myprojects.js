b9f22fa99bff7f1e2cc923bf8d94a81f392b55ef
Mohamed-aldabaan <mohamed.aldabaan@dci-student.org>
2025-01-29 14:56:59 +0100

    Aktualisierung des GitHub-Pfads für das WebDev Roadmap-Projekt in der myprojects.js-Datei

--- my-portfolio/src/myprojects.js
+++ my-portfolio/src/myprojects.js
@@ -6,7 +6,7 @@ export default [
 
     { id: 3, title: 'To-Do-List ', image: '/todo-p.png', description: '16.6.2024 ', category: 'react', githupPath:"https://github.com/Mohamed-aldabaan/ToDoList-Kalender", link:"https://to-do-list-kalender.vercel.app/" },
 
-    { id: 1, 'title': 'WebDev Roadmap', 'image': '/p1.jpg', description: '06.02.2024', category: 'html & css', githupPath:"https://github.com/Mohamed-aldabaan/DCI-Fellow-Program-Plattform", link:"https://developer-roadmaps.vercel.app/" },
+    { id: 1, 'title': 'WebDev Roadmap', 'image': '/p1.jpg', description: '06.02.2024', category: 'html & css', githupPath:"https://github.com/Mohamed-aldabaan/Developer-Roadmaps", link:"https://developer-roadmaps.vercel.app/" },
 
     { id: 2, title: 'Rezept-Projekt', image: '/rezept-app.png', description: '02.09.2024', category: 'mern', githupPath:"https://github.com/Mohamed-aldabaan/Rezept-Projekt", link:"https://github.com/Mohamed-aldabaan/DCI-Fellow-Program-Plattform" },
     { id: 7, title: 'Fellow-Program-Plattform', image: '/fellow.png', description: '13.01.2024', category: 'mern', githupPath:"https://github.com/Mohamed-aldabaan/DCI-Fellow-Program-Plattform", link:"https://github.com/Mohamed-aldabaan/DCI-Fellow-Program-Plattform" },
